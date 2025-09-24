51b3fc7a17c7afe3ceb4369b586a812e6050cf17
BashirAhmed-cse <bashirahmed.cse@gmail.com>
2025-09-24 18:12:03 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,16 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  // output: 'export',
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default withNextIntl(nextConfig);
